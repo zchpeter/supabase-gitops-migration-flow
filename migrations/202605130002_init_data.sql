692dd4ea1c7f95468b7b824feb23f5e449b036ae
zchpeter <zchpeter@gmail.com>
2026-05-13 06:16:48 -0400

    fix: on duplicate -> on conflict

--- migrations/202605130002_init_data.sql
+++ migrations/202605130002_init_data.sql
@@ -1,1 +1,1 @@
-INSERT INTO "public"."users" (name, email, phone) VALUES ('Bytebase', 'help@bytebase.com', '123456789') ON DUPLICATE KEY IGNORE;
+INSERT INTO "public"."users" (name, email, phone) VALUES ('Bytebase', 'help@bytebase.com', '123456789') ON CONFLICT DO NOTHING;
