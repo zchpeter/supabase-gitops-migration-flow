e3545edf3c2792cef8ee212efe4fa3adc2131d6d
zchpeter <zchpeter@gmail.com>
2026-05-13 06:05:15 -0400

    fix: duplicate insert

--- migrations/202605130002_init_data.sql
+++ migrations/202605130002_init_data.sql
@@ -1,1 +1,1 @@
-INSERT INTO "public"."users" (name, email, phone) VALUES ('Bytebase', 'help@bytebase.com', '123456789');
+INSERT INTO "public"."users" (name, email, phone) VALUES ('Bytebase', 'help@bytebase.com', '123456789') ON DUPLICATE KEY IGNORE;
